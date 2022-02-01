93ebcfa91295ec991c24a8e8525885456acf420f
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2022-02-02 00:32:50 +0530

    db connecting

--- server.js
+++ server.js
@@ -9,13 +9,9 @@ const ImageController = require("./Controllers/ImageController");
 const app = express();
 
 const db = knex({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1", 
-    user: "postgres", 
-    port: 5432, 
-    password: "test", 
-    database: "smartbrain", 
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   },
 });
 
